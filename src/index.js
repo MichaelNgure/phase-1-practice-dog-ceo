db0066507c3642e319a6889965f21c58fafb9fa5
MichaelNgure <mikynjoro01@gmail.com>
2024-07-13 14:12:13 +0300

    dog ceo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 console.log('%c HI', 'color: firebrick')
+async function getDoggo(event) {
+    const res = await fetch("https://dog.ceo/api/breeds/image/random");
+    const resJson = await res.json();
+    document.getElementById("dog").src = resJson.message;
+  }
+
+  document.getElementById('get-doggo')
+    .addEventListener('click', getDoggo)
